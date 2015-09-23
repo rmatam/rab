f7c19ccb8d61a523e606d7bc56fb8444d55f39e1
Steve McCauley <steven_mccauley@brown.edu>
2015-09-23 14:24:48 -0400

    Goofing around with SearchTermNames

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ set -e
 /etc/init.d/tomcat7 stop
 ant deploy -Dskiptests=true
 chown -R tomcat7:tomcat7 /var/lib/tomcat7/webapps/rab*
-chown -R tomcat7:tomcat7 /home/vagrant/rab-vitro/data-home/
+chown -R tomcat7:tomcat7 /usr/local/rab-vitro/data
 /etc/init.d/tomcat7 start
